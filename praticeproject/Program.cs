26184c2c6899fc09662b4a483985ab2b452459f5
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-12 15:23:17 +0530

    added file

---
+++ praticeproject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace praticeproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello INDIA!");
+        }
+    }
+}
